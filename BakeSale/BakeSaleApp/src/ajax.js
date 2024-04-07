bad4508abfcdc997ca04466ce5c0d5283000a997
JCarlosMtzZ <juancmtz777@gmail.com>
2024-04-07 00:05:21 -0600

    stylingdeal details

--- BakeSale/BakeSaleApp/src/ajax.js
+++ BakeSale/BakeSaleApp/src/ajax.js
@@ -3,8 +3,17 @@ const apiHost = 'https://bakesaleapi-rn.onrender.com';
 export default {
   async fetchInitialDeals() {
     try {
-      let response = await fetch(apiHost + '/deals');
-      let responseJson = await response.json();
+      const response = await fetch(apiHost + '/deals');
+      const responseJson = await response.json();
+      return responseJson
+    } catch(error) {
+      console.error(error);
+    }
+  },
+  async fetchDealDetail(dealId) {
+    try {
+      const response = await fetch(apiHost + '/deals/' + dealId);
+      const responseJson = await response.json();
       return responseJson
     } catch(error) {
       console.error(error);
--- BakeSale/BakeSaleApp/src/components/DealDetail.js
+++ BakeSale/BakeSaleApp/src/components/DealDetail.js
@@ -1,57 +1,90 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import { priceDisplay } from '../util';
+import ajax from '../ajax';
 
 function DealDetail({ initialDealData }) {
 
-    const [deal, setDeal] = useState(initialDealData);
+  const [deal, setDeal] = useState(initialDealData);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await ajax.fetchDealDetail(initialDealData.key);
+      setDeal(data);
+    } 
+    
+    fetchData();
+
+  }, []);
+
+  
 
   return (
     <View style={styles.container}>
       <Image source={{ uri: deal.media[0] }}
         style={styles.image} 
       />
-      <View style={styles.info}>
-        <Text style={styles.title}>{deal.title}</Text>
-        <View style={styles.footer}>
-          <Text>{priceDisplay(deal.price)}</Text>
-          <Text>{deal.cause.name}</Text>
+      <Text style={styles.title}>{deal.title}</Text>
+      {deal.user && (
+        <View style={styles.header}>
+          <View style={styles.subHeader}>
+            <Text>{priceDisplay(deal.price)}</Text>
+            <Text>{deal.cause.name}</Text>
+          </View>
+          <View style={styles.subHeader}>
+            <Image source={{ uri: deal.user.avatar }} style={styles.avatar}/>
+            <Text>{deal.user.name}</Text>
+          </View>
         </View>
+      )}
+      <View>
+        <Text style={styles.description}>{deal.description}</Text>
       </View>
-      <Text>These are the datails</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: 'auto',
+    flex: 1,
+    width: '100%',
+    height: 'fit-content',
     marginHorizontal: 15,
-    marginTop: 15
-
-  },
-  info: {
-    padding: 15,
-    backgroundColor: '#fff',
+    marginTop: 15,
     borderColor: '#bbb',
     borderWidth: 1,
-    borderTopWidth: 0
   },
   title: {
+    padding: 15,
     fontWeight: 'bold',
     fontSize: 18,
     marginBottom: 5,
-    textAlign: 'justify'
+    textAlign: 'justify',
+    backgroundColor: '#eee'
   },
   image: {
     width: '100%',
     height: 150,
     backgroundColor: '#ccc'
   },
-  footer: {
-    fontSize: 14,
-    flexDirection: 'row-reverse',
-    justifyContent: 'space-between'
+  header: {
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
+  },
+  subHeader: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: '50%'
+  },
+  description: {
+    textAlign: 'justify',
+    padding: 15
   }
 });
 
